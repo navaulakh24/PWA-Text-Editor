c525a0fe66cbb80e726a5189be3af4e5abe78fa1
Nav <navdeep_aulakh24@hotmail.com>
2023-03-16 21:31:45 -0400

    added css loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -47,7 +47,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+          },
+        },
       ],
     },
   };
