3fbee7fc761f53d67b646e20feaeebedcfc1fa99
Nav <navdeep_aulakh24@hotmail.com>
2023-03-16 21:29:58 -0400

    working on workbox plugin in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,31 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        title: 'PWA Text Editor',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'PWA Text Editor',
+        short_name: 'PWA Editor',
+        description: 'A simple text editor PWA',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
